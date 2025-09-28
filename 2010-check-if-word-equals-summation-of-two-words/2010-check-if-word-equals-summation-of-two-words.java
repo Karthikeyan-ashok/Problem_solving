4fd53aff552521244da8450b3938be6af07c88ca
Karthikeyan <karthiknashokkumar@gmail.com>
2025-09-28 16:22:17 +0530

    Time: 0 ms (100%), Space: 41.4 MB (86.72%) - LeetHub

---
+++ 2010-check-if-word-equals-summation-of-two-words/2010-check-if-word-equals-summation-of-two-words.java
@@ -0,0 +1,30 @@
+class Solution {
+    int[]arr = {0,1,2,3,4,5,6,7,8,9};
+    public boolean isSumEqual(String firstWord, String secondWord, String targetWord) {
+        int sum1 = 0;
+        for(char ch : firstWord.toCharArray())
+        {
+            sum1 = sum1*10+(ch-'a');
+        }
+
+        int sum2 = 0;
+        for(char ch : secondWord.toCharArray())
+        {
+            sum2 = sum2*10+(ch-'a');
+        }
+        String s = String.valueOf(sum1+sum2);
+        while(s.length()<targetWord.length())
+        {
+            s = "0"+s;
+        }
+        for(int i=0;i<s.length();i++)
+        {
+            char ch = (char)((targetWord.charAt(i)-'a')+'0');
+            if(s.charAt(i)!=ch)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
