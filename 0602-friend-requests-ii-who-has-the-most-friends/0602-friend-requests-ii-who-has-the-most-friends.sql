835cdf95e6d4d3a0ba1285e19061ab6c0c9c43d8
Karthikeyan <karthiknashokkumar@gmail.com>
2025-10-15 18:08:39 +0530

    Time: 273 ms (88.74%), Space: 0B (100%) - LeetHub

---
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,11 @@
+select id, count(id) num 
+from (
+    select requester_id as id
+    from RequestAccepted
+    union all
+    select accepter_id as id
+    from RequestAccepted
+) as a
+group by id
+order by count(id) desc
+limit 1;
