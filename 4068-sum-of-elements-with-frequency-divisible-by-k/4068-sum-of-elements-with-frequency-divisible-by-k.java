e5501d03a11dc633261d8c9fdbfa5fdd3acdb0e5
Karthikeyan <karthiknashokkumar@gmail.com>
2025-10-18 10:34:09 +0530

    Time: 1 ms (99.96%), Space: 42.6 MB (78.8%) - LeetHub

--- 4068-sum-of-elements-with-frequency-divisible-by-k/4068-sum-of-elements-with-frequency-divisible-by-k.java
+++ 4068-sum-of-elements-with-frequency-divisible-by-k/4068-sum-of-elements-with-frequency-divisible-by-k.java
@@ -1,19 +1,43 @@
 class Solution {
     public int sumDivisibleByK(int[] nums, int k) {
-        HashMap<Integer,Integer> map = new HashMap<>();
+        // HashMap<Integer,Integer> map = new HashMap<>();
 
+        // for(int i : nums)
+        // {
+        //     map.put(i,map.getOrDefault(i,0)+1);
+        // }
+
+        // int sum = 0;
+        // for(int i : map.keySet())
+        // {
+        //     int freq = map.get(i);
+        //     if(freq%k==0)
+        //     {
+        //         sum += (i*freq);
+        //     }
+        // }
+
+        // return sum;
+
+        int max = 0;
         for(int i : nums)
         {
-            map.put(i,map.getOrDefault(i,0)+1);
-        }
-
-        int sum = 0;
-        for(int i : map.keySet())
-        {
-            int freq = map.get(i);
-            if(freq%k==0)
+            if(i>max)
             {
-                sum += (i*freq);
+                max = i;
+            }
+        }
+        int[] freq = new int[max+1];
+        for(int i : nums)
+        {
+            freq[i]++;
+        }
+        int sum = 0;
+        for(int i=0;i<freq.length;i++)
+        {
+            if(freq[i]>0 && freq[i]%k==0)
+            {
+                sum += (i*freq[i]);
             }
         }
 
