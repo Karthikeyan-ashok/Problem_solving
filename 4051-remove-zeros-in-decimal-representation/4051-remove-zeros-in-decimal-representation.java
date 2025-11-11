3382a2504bf542f8844ce06ce18150997fffce20
Karthikeyan <karthiknashokkumar@gmail.com>
2025-11-11 13:58:03 +0530

    Time: 8 ms (10.63%), Space: 44.1 MB (6.71%) - LeetHub

---
+++ 4051-remove-zeros-in-decimal-representation/4051-remove-zeros-in-decimal-representation.java
@@ -0,0 +1,9 @@
+class Solution {
+    public long removeZeros(long n) {
+        String s = String.valueOf(n);
+
+        String a = s.replaceAll("0","");
+        
+        return Long.valueOf(a);
+    }
+}
