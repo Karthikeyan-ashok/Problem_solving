7565690744a951b1d0c66f708899c9177e23009d
Karthikeyan <karthiknashokkumar@gmail.com>
2025-11-26 14:47:43 +0530

    Time: 0 ms (100%), Space: 13.3 MB (91.59%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def search(self, nums, target):
+        
+        i = 0
+        j = len(nums)-1
+
+        while i <= j :
+            mid = (i+j)//2
+
+            if nums[mid] == target :
+                return mid
+            elif nums[mid] < target :
+                i = mid+1
+            elif nums[mid] > target :
+                j = mid-1
+        
+        return -1
